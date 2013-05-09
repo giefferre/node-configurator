47cabed449b2ec1c6207385813434938946b3d54
Gianfranco Reppucci <gianfranco.reppucci@gmail.com>
2013-05-09 11:19:19 +0200

    Fix on directory name

--- fabfile.py
+++ fabfile.py
@@ -7,10 +7,13 @@ from fabric.contrib.files import exists
 
 env.warn_only = True
 
+FULL_PATH, _ = os.path.split(os.path.realpath(__file__))
+_, DIRECTORY_NAME = os.path.split(FULL_PATH)
+
 def update_modules():
 	print(blue("Syncing files...", bold=True))
 	rsync_project("/tmp/")
-	sudo('cp /tmp/server-configuration/puppet/modules/* /etc/puppet/modules/ -R')
+	sudo("cp /tmp/%s/puppet/modules/* /etc/puppet/modules/ -R" % DIRECTORY_NAME)
 
 
 @task(alias='up')
@@ -38,7 +41,7 @@ def init():
 		update_modules()
 
 		print(blue("Applying base manifest...", bold=True))
-		sudo('puppet apply /tmp/server-configuration/puppet/base.pp')
+		sudo("puppet apply /tmp/%s/puppet/base.pp" % DIRECTORY_NAME)
 
 		sudo('pip install pycrypto==2.6 paramiko==1.10.1 Fabric==1.6.0')
 
